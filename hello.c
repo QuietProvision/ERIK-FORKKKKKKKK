5150bfac0052615a958e82ce42276e6239853c8f
EB-codeC <erik.bryant@rdgstudent.com>
2026-06-09 15:59:25 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int x = 5;
+    printf("Hello, world!\n");
+    printf("x is = %d\n", x);
+
+    int *array = malloc(sizeof(int) * 20);
+    if (array == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 20; i++) {
+        array[i] = i + 1;
+    }
+
+    printf("First 20 integers allocated with malloc:\n");
+    for (int i = 0; i < 20; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    free(array);
+    return 0;
+}
